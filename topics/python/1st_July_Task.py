ff00c282d03f755bfb95d510e6baea0dfc7de9bc
Tanishka Parekh <parekhtanishka@gmail.com>
2025-07-03 16:44:08 +0530

    pandas task

--- topics/python/1st_July_Task.py
+++ topics/python/1st_July_Task.py
@@ -8,15 +8,17 @@ A1 = np.array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22
 print(A.shape)
 A2 = np.array([[(np.mean(A,axis = 1))[0]],[(np.mean(A,axis = 1))[1]],[(np.mean(A,axis = 1))[2]],[(np.mean(A,axis = 1))[3]],[(np.mean(A,axis = 1))[4]]])
 print(A1-A2)
+print()
 
 print("Answer 2")
 B1 = np.arange(0,1000,21)
 B1 = B1[B1%5 != 0]
 print(B1)
-#or
+print("or") #or
 B=np.arange(1,1000)
 B1 = B[(B%7==0)&(B%3==0)&(B%5!=0)]
 print(B1)
+print()
 
 print("Answer 3")
 C1 = np.zeros((8,1))
@@ -27,8 +29,27 @@ C=np.concatenate((C,C),axis=1)
 print(C)
 
 # Pandas (Create a DataFrame with the specified columns and perform the operations)
-import pandas as pd
+import pandas as pad
 # Q1: Given a DataFrame df with columns: ["department", "employee", "salary"], normalize the salary within each department (i.e., for each department, subtract the mean and divide by the std of that department).
 # Q2: Given a DataFrame with columns ["timestamp", "user_id", "action"], where timestamp is in string format, find the average number of actions per user per day.
 # Q3: You have a DataFrame with columns: ["user_id", "product", "price", "quantity", "date"]. Calculate the total amount spent by each user on "Laptop" purchases only, and return the result as a new DataFrame with columns: ["user_id", "total_spent_on_laptops"].
+print("Answer 1")
+df = pad.DataFrame({
+    "Department":["IT","HR","Admin","Admin","IT","IT","IT","HR","Admin","HR",],
+    "Employee":["E1","E2","E3","E4","E5","E6","E7","E8","E9","E10"],
+    "Salary":[2000000,400000,80000,90000,3000000,1800000,2500000,300000,75000,350000]
+})
+df["Normalise"]=df.groupby("Department")["Salary"].transform(lambda x:(x-x.mean())/x.std())
+print(df)
+print()
+print("Answer 3")
+df=pad.DataFrame({"user_id":["U1","U2","U3","U4","U5","U6"],
+                  "product":["Laptop","Iphone","Laptop","Iphone","Laptop","Laptop"],
+                  "price":[200000,100000,300000,100000,200000,300000],
+                  "quantity":[2,1,3,2,1,4],
+                  "date":["2025-07-01","2025-07-11","2025-07-05","2025-07-04","2025-07-02","2025-07-10"]})
+df2=pad.DataFrame()
+df2=df[df["product"]=="Laptop"].assign(total_spent_on_laptopns =df['price']*df['quantity'])
+df2 = df2.drop(columns=['product','date','price','quantity'])
+print(df2)
 
